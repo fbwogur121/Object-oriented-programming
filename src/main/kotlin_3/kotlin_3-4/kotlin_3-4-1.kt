bb517bf01987e39718c47f67957369b0cf941bc1
RYU <fbwogur121@naver.com>
2024-10-08 09:39:39 +0900

    kotlin_3-4-1 while->do-while

--- src/main/kotlin_3/kotlin_3-4/kotlin_3-4-1.kt
+++ src/main/kotlin_3/kotlin_3-4/kotlin_3-4-1.kt
@@ -11,16 +11,29 @@ package main.kotlin_3.`kotlin_3-4`
 //    println("sum = $sum")
 //}
 
+//import kotlin.random.Random
+//
+//fun main(){
+//    val num = Random.nextInt(1, 101)
+//    var guess = 0
+//
+//    while(guess != num){
+//        guess = readLine()!!.toInt()
+//        if(guess < num) println("Too small")
+//        if(guess > num) println("Too large")
+//    }
+//    println("Right! the number is $num")
+//}
+
 import kotlin.random.Random
 
 fun main(){
     val num = Random.nextInt(1, 101)
-    var guess = 0
 
-    while(guess != num){
-        guess = readLine()!!.toInt()
+    do{
+        val guess = readLine()!!.toInt()
         if(guess < num) println("Too small")
         if(guess > num) println("Too large")
-    }
+    }while (guess != num)
     println("Right! the number is $num")
 }
