257c0642f298e04f16bde5890089fc911eaa665e
RYU <fbwogur121@naver.com>
2024-10-08 09:36:41 +0900

    kotlin_3-4-1 while문

--- src/main/kotlin_3/kotlin_3-4/kotlin_3-4-1.kt
+++ src/main/kotlin_3/kotlin_3-4/kotlin_3-4-1.kt
@@ -1,12 +1,26 @@
 package main.kotlin_3.`kotlin_3-4`
 
+//fun main(){
+//    var sum = 0
+//
+//    do {
+//        val num = readLine()!!.toInt()
+//        sum += num
+//    } while (num != 0)
+//
+//    println("sum = $sum")
+//}
+
+import kotlin.random.Random
+
 fun main(){
-    var sum = 0
+    val num = Random.nextInt(1, 101)
+    var guess = 0
 
-    do {
-        val num = readLine()!!.toInt()
-        sum += num
-    } while (num != 0)
-
-    println("sum = $sum")
+    while(guess != num){
+        guess = readLine()!!.toInt()
+        if(guess < num) println("Too small")
+        if(guess > num) println("Too large")
+    }
+    println("Right! the number is $num")
 }
