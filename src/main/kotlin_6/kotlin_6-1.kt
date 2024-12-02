90b6b76c51040793a8fb3a6753ca925b12cf276b
RYU <fbwogur121@naver.com>
2024-12-02 17:40:46 +0900

    6-1,2

---
+++ src/main/kotlin_6/kotlin_6-1.kt
@@ -0,0 +1,36 @@
+package main.kotlin_6
+
+enum class WeekDay {
+    Monday,
+    Tuesday,
+    WednesdDay,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+fun WeekDay.isWorkDay() =
+    this == WeekDay.Sunday || this == WeekDay.Saturday
+
+enum class Direction {
+    NORTH, SOUTH, EAST, WEST
+}
+
+fun rotateClockwise(direction: Direction) = when(direction){
+    Direction.NORTH -> Direction.EAST
+    Direction.EAST -> Direction.SOUTH
+    Direction.SOUTH -> Direction.WEST
+    Direction.WEST -> Direction.NORTH
+}
+
+fun main(){
+    println(WeekDay.Sunday.isWorkDay())
+    println(WeekDay.Monday.isWorkDay())
+
+    val dir = Direction.NORTH
+    println(rotateClockwise(dir))
+    val dir2 = rotateClockwise(dir)
+    println(rotateClockwise(dir2))
+}
+
---
+++ src/main/kotlin_6/kotlin_6-2.kt
@@ -0,0 +1,23 @@
+package main.kotlin_6
+
+data class Person(val firstName: String,
+            val familyName: String,
+            val age: Int)
+
+fun Person.show() = println("$firstName $familyName: $age")
+
+fun main() {
+    val person1 = Person("John", "Doe", 25)
+    val person2 = Person("John", "Doe", 25)
+    val person3 = person1
+
+    println(person1 == person2)
+    println(person1 == person3)
+
+    person1.show() // John Doe: 25
+    person1.copy().show() // John Doe: 25
+    person1.copy(familyName = "Smith").show() // John Smith: 25
+    person1.copy(age = 30, firstName = "Jane").show() // Jane Doe: 30
+    person1.show() // John Doe: 25
+    person1.copy().show() // John Doe: 25
+}
